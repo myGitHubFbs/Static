dc30683951805067bd73482d28ab59be4ac6fd72
KiKo 'S <kiko0272@hotmail.com>
2026-02-19 07:38:57 -0600

    Act

--- obj/Debug/net7.0/Static.AssemblyInfo.cs
+++ obj/Debug/net7.0/Static.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Static")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b6f728cff568c5fe87070c04df32cbb93349b3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92341e5dc434cf3fbff928d031605837fe034e7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Static")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Static")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
